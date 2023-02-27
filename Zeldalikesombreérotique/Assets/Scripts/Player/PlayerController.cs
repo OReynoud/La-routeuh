0d252c208df829a9f9374f7916ae1263aa1bca0d
Oscar Reynoud <o.reynoud@rubika-edu.com>
2023-02-27 10:35:06 +0100

    Feurst

---
+++ Zeldalikesombreérotique/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using NaughtyAttributes;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Foldout("Références")]public Rigidbody rb;
+    [Header("Mouvements")]
+    [Space(10)]
+    [BoxGroup]public float groundSpeed;
+
+    [Foldout("Débug")] public Vector3 playerDir;
+    [Foldout("Débug")] public bool isGrounded;
+    
+    private InputManager controls;
+    void Awake()
+    {
+        controls = new InputManager();
+        controls.Player.Move.performed += ctx => Move(ctx.ReadValue<Vector2>());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!controls.Player.Move.IsPressed() && isGrounded)
+        {
+            playerDir = new Vector3(playerDir.x * 0.1f,playerDir.y,playerDir.z * 0.1f);
+            rb.velocity = playerDir;
+            rb.angularVelocity = Vector3.zero;
+        }
+        else
+        {
+            RotateModel();
+            rb.velocity = playerDir * groundSpeed;
+        }
+    }
+
+    private void Move(Vector2 dir)
+    {
+        playerDir = new Vector3(dir.x,playerDir.y, dir.y);
+    }
+
+    private void RotateModel()
+    {
+        var angle = Mathf.Atan2(playerDir.x, playerDir.z)* Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle,Vector3.up);
+        Debug.Log(angle);
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+}
