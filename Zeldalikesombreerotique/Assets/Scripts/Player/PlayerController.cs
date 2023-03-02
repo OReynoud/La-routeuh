fabe1197b68fc3ed0cb244d1af5471721ee292b4
Oscar Reynoud <o.reynoud@rubika-edu.com>
2023-03-02 17:39:47 +0100

    le controller est chiant

--- Zeldalikesombreerotique/Assets/Scripts/Player/PlayerController.cs
+++ Zeldalikesombreerotique/Assets/Scripts/Player/PlayerController.cs
@@ -12,6 +12,8 @@ namespace Player
         [Header("Mouvements")]
         [Space(10)]
         [BoxGroup][Tooltip("Vitesse du joueur")]public float groundSpeed;
+        [BoxGroup][Tooltip("Vitesse du joueur")]public float minSpeed;
+        [BoxGroup][Tooltip("Vitesse du joueur")]public float maxSpeed;
 
         [BoxGroup] public float grabedRotateSpeed;
 
@@ -19,8 +21,8 @@ namespace Player
         [Foldout("Débug")][Tooltip("Est-ce que le joueur touche le sol?")] public bool isGrounded;
         [Foldout("Débug")][Tooltip("Est-ce que le jeu fait des trucs de gros shlag pour la PoC?")] public bool proofOfConcept;
         [Foldout("Débug")][Tooltip("Est-ce que je joueur manipule un objet?")] public bool isGrabing;
-        [Foldout("Débug")] public bool canGrab;
-        [Foldout("Débug")] public Rigidbody objectToGrab;
+        [Foldout("Débug")][Tooltip("Y a t-il un objet à porté que le joueur peut grab?")] public bool canGrab;
+        [Foldout("Débug")][Tooltip("Quel est l'objet à grab")] public Rigidbody objectToGrab;
     
         private InputManager controls;
         private float _baseOffset = -2.5f;
@@ -52,11 +54,12 @@ namespace Player
             else if(!isGrabing)
             {
                 RotateModel();
-                rb.velocity = playerDir * groundSpeed;
+                rb.AddForce(playerDir * (groundSpeed),ForceMode.Force);
             }
             else if (isGrabing)
             {
-                
+                RotateModel();
+                rb.AddForce(playerDir * (groundSpeed),ForceMode.Force);
             }
         }
 
@@ -70,7 +73,7 @@ namespace Player
         }
         private void OnTriggerExit(Collider other)
         {
-            if (other.CompareTag("Moveable"))
+            if (other.CompareTag("Moveable") && !isGrabing)
             {
                 canGrab = false;
                 objectToGrab = null;
@@ -110,12 +113,20 @@ namespace Player
 
             Vector3 dir = objectToGrab.transform.position - transform.position;
             var dirNormed = dir.normalized;
-            joint.connectedAnchor = new Vector3(dirNormed.x, 0, dirNormed.z);
-            joint.anchor = transform.position;
+            //joint.connectedAnchor = new Vector3(dirNormed.x, 0, dirNormed.z);
+            //joint.anchor = transform.position;
             var angle2 = Mathf.Atan2(dirNormed.x, dirNormed.z) * Mathf.Rad2Deg;
             //transform.rotation = Quaternion.AngleAxis(angle2,Vector3.up);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation,Quaternion.AngleAxis(angle2,Vector3.up),grabedRotateSpeed);
+            transform.rotation = Quaternion.AngleAxis(angle2,Vector3.up);
         }
+
+        /*private void ResetVelocity()
+        {
+            if (rb.velocity.normalized - playerDir >= Vector3.one * 0.3f)
+            {
+                
+            }
+        }*/
 
         private void OnEnable()
         {
