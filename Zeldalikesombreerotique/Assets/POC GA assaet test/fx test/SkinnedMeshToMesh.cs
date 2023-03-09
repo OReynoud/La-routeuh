c9bcb876d1c30384ff204a13ba503159b559856a
Fraise <s.bouveret@rubika-edu.com>
2023-03-09 15:50:38 +0100

    vfx graph test

---
+++ Zeldalikesombreerotique/Assets/POC GA assaet test/fx test/SkinnedMeshToMesh.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class SkinnedMeshToMesh : MonoBehaviour
+{
+    public MeshRenderer 
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
