a0c0808bdd11ffb5a1fc50b06062f56a7286be76
Tintin <v.rousselle@rubika-edu.com>
2023-03-18 23:14:03 +0100

    Création du prefab Shadow

---
+++ Zeldalikesombreerotique/Assets/Scripts/Utilities/Shadow.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace Utilities
+{
+    public class Shadow : MonoBehaviour
+    {
+        [Tooltip("Point where the player will respawn if they are killed by the shadow")] [SerializeField] internal Transform respawnPoint;
+    
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                other.gameObject.transform.position = respawnPoint.position;
+            }
+        }
+    }
+}
