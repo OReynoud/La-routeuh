956373745ca8d00b64b3c0ef446b7c76fd16cc4a
Tintin <v.rousselle@rubika-edu.com>
2023-03-19 22:55:19 +0100

    Vrai début proposition 1

--- Zeldalikesombreerotique/Assets/Scripts/Utilities/LoopingMap.cs
+++ Zeldalikesombreerotique/Assets/Scripts/Utilities/LoopingMap.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Utilities
@@ -6,12 +7,18 @@ namespace Utilities
     {
         [SerializeField] private Transform newPosition;
         [SerializeField] private GameObject mapPartToMove;
+        [SerializeField] private List<GameObject> mapPartsToDisable = new();
     
         private void OnTriggerEnter(Collider other)
         {
             if (other.CompareTag("Player"))
             {
                 mapPartToMove.transform.position = newPosition.position;
+                mapPartToMove.SetActive(true);
+                foreach (var mapPart in mapPartsToDisable)
+                {
+                    mapPart.SetActive(false);
+                }
             }
         }
     }
