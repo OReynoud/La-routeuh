4095da45d0b493823a28ca0a079c62334a12ce4a
DJ_Yazid <y.djaoui@rubika-edu.com>
2023-03-20 12:37:48 +0100

    Du bon vieille UI

---
+++ Zeldalikesombreerotique/Assets/GroupeFolder/Yazid/MainMenu.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public string[] SceneName;
+    public int SceneValue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneValue = PlayerPrefs.GetInt("TheValue", SceneValue);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            SceneValue += 1;
+            if(SceneValue >= 4)
+            {
+                SceneValue = 0;
+            }
+            PlayerPrefs.SetInt("TheValue", SceneValue);
+            SceneManager.LoadScene(SceneName[SceneValue]);
+        }
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("PuzzleGD");
+    }
+}
