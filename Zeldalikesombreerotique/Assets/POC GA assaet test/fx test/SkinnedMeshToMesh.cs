ac6c58dceaac0f18defd86c5ff68df33fca069d3
Oscar Reynoud <o.reynoud@rubika-edu.com>
2023-04-11 15:04:54 +0200

    fix temporaire des pics de lag

--- Zeldalikesombreerotique/Assets/POC GA assaet test/fx test/SkinnedMeshToMesh.cs
+++ Zeldalikesombreerotique/Assets/POC GA assaet test/fx test/SkinnedMeshToMesh.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.VFX;
 
@@ -8,16 +10,39 @@ public class SkinnedMeshToMesh : MonoBehaviour
     public SkinnedMeshRenderer Beta_Surface;
     public VisualEffect VFXGraph;
     public float refreshRate;
+    private float timer = 0;
     private Mesh m;
     private Mesh m2;
+    private List<Vector3> vertices = new List<Vector3>();
     void Start()
     {
         m = new Mesh();
         m2 = new Mesh();
-        StartCoroutine(UpdateVFXGraph());
+        //StartCoroutine(UpdateVFXGraph());
     }
 
-    IEnumerator UpdateVFXGraph()
+   private void FixedUpdate()
+    {
+        if (timer < refreshRate)
+        {
+            timer += Time.deltaTime;
+        }
+        else
+        {
+            
+            Beta_Surface.BakeMesh(m);
+            /*vertices.Clear();
+            vertices.AddRange(m.vertices);
+            m2.vertices.AddRange(vertices);*/
+            Vector3[] vertices = m.vertices;
+            m2.vertices = vertices;
+            
+            VFXGraph.SetMesh("Mesh", m2);
+            timer = 0;
+        }
+    }
+
+    /*IEnumerator UpdateVFXGraph()
     {
         while (gameObject.activeSelf)
         {
@@ -30,5 +55,5 @@ public class SkinnedMeshToMesh : MonoBehaviour
             
             yield return new WaitForSeconds (refreshRate);
         }
-    }
+    }*/
 }
