2bd71c1466865211fd562397ead405d7953404b9
TheTintinXY <99655918+TheTintinXY@users.noreply.github.com>
2023-04-17 22:39:39 +0200

    Petites modifs

--- Zeldalikesombreerotique/Assets/Scripts/Utilities/PresenceStartAnimation.cs
+++ Zeldalikesombreerotique/Assets/Scripts/Utilities/PresenceStartAnimation.cs
@@ -1,23 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
+using NaughtyAttributes;
 using UnityEngine;
 
 namespace Utilities
 {
     public class PresenceStartAnimation : MonoBehaviour
     {
+        private AudioSource _audioSource;
         [SerializeField] private float timeBeforeStart;
         [SerializeField] private GameObject spotToRotate;
         [SerializeField] private float angleToRotateSpot;
         [SerializeField] private float timeToRotateSpot;
         [SerializeField] private Ease easeToRotateSpot;
+        [ValidateInput("IsNotNull", "Don't forget to uncomment the line in code to play the sound.")]
+        [SerializeField] private AudioClip soundToRotateSpot;
         [SerializeField] private float timeBetweenSpotAndFootprints;
         [SerializeField] private List<GameObject> footprintsToAppear;
         [SerializeField] private float timeBetweenFootprints;
+        [ValidateInput("IsNotNull", "Don't forget to uncomment the line in code to play the sound.")] 
+        [SerializeField] private AudioClip soundToAppearFootprints;
 
         private void Awake()
         {
+            _audioSource = GetComponent<AudioSource>();
+            
             foreach (var footprint in footprintsToAppear)
             {
                 footprint.SetActive(false);
@@ -35,6 +43,7 @@ namespace Utilities
         private IEnumerator PresenceStartAnimationCoroutine()
         {
             yield return new WaitForSeconds(timeBeforeStart);
+            // _audioSource.PlayOneShot(soundToRotateSpot);
             spotToRotate.transform
                 .DOLocalRotate(spotToRotate.transform.localEulerAngles + new Vector3(0, angleToRotateSpot, 0),
                     timeToRotateSpot).SetEase(easeToRotateSpot)
@@ -47,8 +56,11 @@ namespace Utilities
             foreach (var footprint in footprintsToAppear)
             {
                 footprint.SetActive(true);
+                // _audioSource.PlayOneShot(soundToAppearFootprints);
                 yield return new WaitForSeconds(timeBetweenFootprints);
             }
         }
+
+        private bool IsNotNull(AudioClip ac) { return ac != null; }
     }
 }
