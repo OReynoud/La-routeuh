998fed34a5d2d9fc1cd63705ae02e7861a8cd098
TheTintinXY <99655918+TheTintinXY@users.noreply.github.com>
2023-05-01 12:58:02 +0200

    Des trucs bidules

--- Zeldalikesombreerotique/Assets/Parkour.cs
+++ Zeldalikesombreerotique/Assets/Parkour.cs
@@ -1,6 +1,6 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Parkour : MonoBehaviour
 {
@@ -8,39 +8,34 @@ public class Parkour : MonoBehaviour
     public List<Transform> controlPoint;
     public List<Transform> endPoint;
 
-    public float speed = 0.01f;
+    [SerializeField] private float speed;
 
-    private float timer = 0;
+    private float _timer;
 
-    private bool triggerCinematic = true;
+    private bool _triggerCinematic;
 
-    public GameObject laPetiteFille;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [FormerlySerializedAs("laPetiteFille")] [SerializeField] private GameObject objectToMove;
 
     // Update is called once per frame
     void Update()
     {
-        if (triggerCinematic && startPoint.Count != 0)
+        if (_triggerCinematic && startPoint.Count != 0)
         {
-            if (timer < 1)
+            if (_timer < 1)
             {
-                timer += speed * Time.deltaTime;
+                _timer += speed * Time.deltaTime;
                 
-                Vector3 m1 = Vector3.Lerp( startPoint[0].position, controlPoint[0].position, timer );
-                Vector3 m2 = Vector3.Lerp( controlPoint[0].position, endPoint[0].position, timer );
+                Vector3 m1 = Vector3.Lerp( startPoint[0].position, controlPoint[0].position, _timer );
+                Vector3 m2 = Vector3.Lerp( controlPoint[0].position, endPoint[0].position, _timer );
 
-                laPetiteFille.transform.position = Vector3.Lerp(m1, m2, timer);
+                objectToMove.transform.position = Vector3.Lerp(m1, m2, _timer);
             }
             else
             {
                 startPoint.RemoveAt(0);
                 controlPoint.RemoveAt(0);
                 endPoint.RemoveAt(0);
-                timer = 0;
+                _timer = 0;
             }
         }
     }
