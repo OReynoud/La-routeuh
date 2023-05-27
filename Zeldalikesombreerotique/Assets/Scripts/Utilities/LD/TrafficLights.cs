dfd9c500207846cd84b7312916972915146c15c5
Tintin <v.rousselle@rubika-edu.com>
2023-05-27 19:01:14 +0200

    Trafffic lights et émission corrigés

--- Zeldalikesombreerotique/Assets/Scripts/Utilities/LD/TrafficLights.cs
+++ Zeldalikesombreerotique/Assets/Scripts/Utilities/LD/TrafficLights.cs
@@ -7,12 +7,25 @@ namespace Utilities.LD
 {
     public class TrafficLights : MonoBehaviour
     {
+        [SerializeField] private MeshRenderer meshRenderer;
         [SerializeField] private GameObject greenLight;
+        [SerializeField] private float greenLightIntensityFactor;
         [SerializeField] private GameObject redLight;
+        [SerializeField] private float redLightIntensityFactor;
         [SerializeField] private List<Draw> appearedDraws;
+        private static readonly int ColorRouge = Shader.PropertyToID("_ColorRouge");
+        private static readonly int ColorVert = Shader.PropertyToID("_ColorVert");
 
         private void Awake()
         {
+            var redColor = meshRenderer.material.GetColor(ColorRouge);
+            meshRenderer.material.SetColor(ColorRouge, 
+                new Color( redColor.r * redLightIntensityFactor, redColor.g * redLightIntensityFactor, redColor.b * redLightIntensityFactor));
+            
+            var greenColor = meshRenderer.material.GetColor(ColorVert);
+            meshRenderer.material.SetColor(ColorVert, 
+                new Color( greenColor.r / greenLightIntensityFactor, greenColor.g / greenLightIntensityFactor, greenColor.b / greenLightIntensityFactor));
+            
             foreach (var draw in appearedDraws)
             {
                 draw.LinkedTrafficLights.Add(this);
@@ -39,6 +52,14 @@ namespace Utilities.LD
         private IEnumerator ChangeLightColor()
         {
             yield return new WaitUntil(() => appearedDraws.All(draw => draw.Links.All(link => link.link.IsDrawn)));
+            
+            var redColor = meshRenderer.material.GetColor(ColorRouge);
+            meshRenderer.material.SetColor(ColorRouge, 
+                new Color( redColor.r / redLightIntensityFactor, redColor.g / redLightIntensityFactor, redColor.b / redLightIntensityFactor));
+            
+            var greenColor = meshRenderer.material.GetColor(ColorVert);
+            meshRenderer.material.SetColor(ColorVert, 
+                new Color( greenColor.r * greenLightIntensityFactor, greenColor.g * greenLightIntensityFactor, greenColor.b * greenLightIntensityFactor));
             
             greenLight.SetActive(true);
             redLight.SetActive(false);
