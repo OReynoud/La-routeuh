f59313a68f177a443a4384e400afbc306a39bb19
Oscar Reynoud <o.reynoud@rubika-edu.com>
2023-06-04 14:56:10 +0200

    On a enfin un menu pause mdr

--- Zeldalikesombreerotique/Assets/PauseMenu.cs
+++ Zeldalikesombreerotique/Assets/PauseMenu.cs
@@ -1,12 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
+using Player;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.UI;
 using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
+    public static PauseMenu instance;
     public  InputSystemUIInputModule input;
     
     public RectTransform optionGroup;
@@ -16,18 +20,28 @@ public class PauseMenu : MonoBehaviour
     
     public float settingsOffset;
     public float controlsOffset;
+
+    public CanvasGroup background;
     // Start is called before the first frame update
-    void Start()
+    public void Awake()
     {
-        
+        if (instance != null)
+        {
+            DestroyImmediate(this);
+        }
+
+        instance = this;
     }
 
     // Update is called once per frame
-    public void ShowOption()
+    public void ShowOption(InputAction.CallbackContext context)
     {
+        PlayerController.instance.introCinematic = true;
+        PlayerController.instance.controls.Disable();
         StartCoroutine(AvoidSpams());
         optionGroup.DOLocalMove(optionGroup.localPosition + Vector3.right * settingsOffset, 0.5f);
         controlsImage.DOLocalMove(controlsImage.localPosition + Vector3.down * controlsOffset, 0.5f);
+        background.DOFade(0.5f, 0.5f);
         backButton.Select();
     }
 
@@ -36,6 +50,10 @@ public class PauseMenu : MonoBehaviour
         StartCoroutine(AvoidSpams());
         optionGroup.DOLocalMove(optionGroup.localPosition + Vector3.left * settingsOffset, 0.5f);
         controlsImage.DOLocalMove(controlsImage.localPosition + Vector3.up * controlsOffset, 0.5f);
+        background.DOFade(0f, 0.5f);
+        
+        PlayerController.instance.introCinematic = false;
+        PlayerController.instance.controls.Enable();
     }
     public IEnumerator AvoidSpams()
     {
